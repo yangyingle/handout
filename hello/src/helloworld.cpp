d43b0fe756aa4e7582a440c5988d17aebcba2083
yangyingle <yang.yingle28@gmail.com>
2013-09-08 15:12:53 +0800

    C++

---
+++ hello/src/helloworld.cpp
@@ -0,0 +1,17 @@
+/*
+ * helloworld.cpp
+ *
+ *  Created on: 2013年9月8日
+ *      Author: yangyingle
+ */
+#include<iOstream>
+using namespace std;
+int main()
+{
+	cout<<"hello world!!,I am 杨营乐。"<<endl;
+	return 0;
+
+}
+
+
+
